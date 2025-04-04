209a5c9b990649ec88d1952e3ad0750afba98cca
kleinalex-01 <kleinalex215@gmail.com>
2025-04-04 13:50:29 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "about", component: AboutComponent},
+    {path: "blog", component: BlogComponent}
+];
